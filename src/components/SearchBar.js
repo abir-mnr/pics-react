1ca6877d532d44280b1ae4fac88ea03165277964
Md Musta E Nur Rahman <mnrabir@gmail.com>
2024-05-05 01:57:49 +0900

    Add the functionality to send the term to parent component for api use

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,25 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+    state = {term: ''};
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        this.props.onSubmit(this.state.term);
+    }
+
     render() {
         return (
-            <div>
-                <form>
-                    Input Text: 
-                    <input type="text" />
+            <div className="ui segment">
+                <form className="ui form" onSubmit={(event) => this.onFormSubmit(event)}>
+                    <div className="field">
+                        <label> Input Text</label>
+                        <input type="text" value={this.state.term} onChange={(e) => {this.setState({term: e.target.value})}}/>
+                    </div>
                 </form>
             </div>
         );
     };
-};
+}
 
 export default SearchBar;
